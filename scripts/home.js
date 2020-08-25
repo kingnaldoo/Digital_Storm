22263eeed6b49e666206c87cb91b1ff465baf3cb
Reinaldo da Silva Nascimento <reinaldosn2016@gmail.com>
2020-08-25 16:43:21 -0300

    responsividade ok

--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,12 @@
 const buttonMenu = document.querySelector("#button")
 const menu = window.document.querySelector('aside')
 const contact = window.document.querySelector('#link-contact')
+const buttonAction = window.document.querySelector('#button-action')
+const header = document.querySelector('header')
+const mobileMenu = document.querySelector('#link-contact a')
 var count = 0
-console.log(pageYOffset)
+
+
 contact.addEventListener('click', function contact_scroll(){
     count+=1
     menu.style.display='none'
@@ -22,3 +26,21 @@ buttonMenu.addEventListener('click', function menuAction(){
         menu.style.transition='0.5s'
     }
 })
+
+window.document.addEventListener('scroll', function rolagem(){
+    
+    if(pageYOffset!=0){
+        header.style.borderBottom = 'solid 1px white'
+    }
+    else{
+        header.style.borderBottom = 'none'
+    }
+
+    if(pageYOffset>=480){
+        mobileMenu.style.display = 'block'
+    }
+    else{
+        mobileMenu.style.display = 'none'
+    }
+})
+
