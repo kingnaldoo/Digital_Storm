6f7705856d7779215606942e19ea5fd878edecf6
Reinaldo da Silva Nascimento <reinaldosn2016@gmail.com>
2020-08-25 00:53:23 -0300

    header e menu flutuante ok

--- scripts/home.js
+++ scripts/home.js
@@ -1,26 +1,22 @@
-const header = document.querySelector('header')
-const mobileMenu = document.querySelector('#link-contact a')
-
-window.document.addEventListener('scroll', function rolagem(){
-    
-    if(pageYOffset!=0){
-        header.style.borderBottom = 'solid 1px white'
-        console.log(pageYOffset)
-    }
-    else{
-        header.style.borderBottom = 'none'
-    }
-
-    if(pageYOffset>=480){
-        mobileMenu.style.display = 'block'
-    }
-    else{
-        mobileMenu.style.display = 'none'
-    }
-
-    if(pageXOffset!=0){
-        pageXOffset=0
-    }
+const buttonMenu = document.querySelector("#button")
+const menu = window.document.querySelector('aside')
+const contact = window.document.querySelector('#link-contact')
+var count = 0
+console.log(pageYOffset)
+contact.addEventListener('click', function contact_scroll(){
+    count+=1
+    menu.style.display='none'
+    buttonMenu.setAttribute('src', 'assets/menu-button.svg')
 })
 
-
+buttonMenu.addEventListener('click', function menuAction(){
+    count+=1
+    if(count%2!=0){ 
+        menu.style.display='block'
+        buttonMenu.setAttribute('src', 'assets/x.svg')
+    }
+    else{
+        menu.style.display = 'none'
+        buttonMenu.setAttribute('src', 'assets/menu-button.svg')
+    }
+})
